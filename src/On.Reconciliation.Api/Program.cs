71f7ee6ee85a6cd7f083b59561a8e856f70f6260
Thomas <thomas.karoliussen@on.no>
2022-12-22 16:10:34 +0100

    Register all services

--- src/On.Reconciliation.Api/Program.cs
+++ src/On.Reconciliation.Api/Program.cs
@@ -13,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
 
 builder.Configuration.AddJsonFile("appSettings.json");
+builder.Configuration.AddUserSecrets<Program>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,17 +27,18 @@ builder.Services.AddTransient<IRuleQueries, RuleQueries>();
 builder.Services.AddTransient<IStatementQueries, StatementQueries>();
 builder.Services.AddTransient<IAccountingClientQueries, AccountingClientQueries>();
 builder.Services.AddTransient<IGeneralLedgerQueries, GeneralLedgerQueries>();
+builder.Services.AddTransient<IVatQueries, VatQueries>();
 
 // commands
 builder.Services.AddTransient<IReconciliationCommands, ReconciliationCommands>();
+builder.Services.AddTransient<IRuleCommands, RuleCommands>();
 
 // services
 builder.Services.AddTransient<IBookingService, BookingService>();
 builder.Services.AddTransient<IMatchingService, MatchingService>();
 builder.Services.AddTransient<IRuleService, RuleService>();
 
-builder.Services.ConfigureMessaging()
-    .Configure<MessageSources>(builder.Configuration.GetSection("MessageSources"));
+builder.Services.ConfigureMessaging().Configure<MessageSources>(builder.Configuration.GetSection("MessageSources"));
 
 var app = builder.Build();
 
