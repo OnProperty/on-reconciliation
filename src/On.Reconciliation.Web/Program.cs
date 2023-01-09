569de80c567d1778c5b0739562a58ffa79514968
Thomas <thomas.karoliussen@on.no>
2023-01-09 18:31:20 +0100

    Add appsettings, use it for api base url

--- src/On.Reconciliation.Web/Program.cs
+++ src/On.Reconciliation.Web/Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Configuration.AddJsonFile("appsettings.json");
+var apiBaseAddress = builder.Configuration["ApiBaseUrl"];
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseAddress) });
 
 await builder.Build().RunAsync();
